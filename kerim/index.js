bdb28bdd85b20f5fbaf0b2a9185fe1c7035e56fc
memmedkerim taghiyev <7m16zu6@code.edu.az>
2023-04-11 15:43:53 +0300

    first

---
+++ kerim/index.js
@@ -0,0 +1,80 @@
+// let factorial=5
+// let cavab = 1
+// for(i=1; i<=factorial; i++){
+//     cavab*=i
+// }
+// console.log(cavab);
+
+// for(i=1; i<=100; i++){
+//     if(i%6==0){
+//         console.log(i);
+//     }
+// }
+
+// let a=12;
+// let cem=0;
+
+// for(let i=0; i<=a; i++ ){
+//     if(a%i==0){
+//         console.log(i);
+//         cem=cem+1;
+//     }
+
+// }console.log(cem);
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 3 || i == 5 || i == 7) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let eded,cem,hasil,i;
+// eded=8;
+// cem=0;
+// hasil=1;
+
+// for(i=1; i<=eded; i++){
+//     cem+=i;
+//     hasil*=i;
+//     console.log(i,"-ci addim");
+//     console.log("cem: ", cem);
+//     console.log("hasil: ", hasil);
+// }
+
+// let cem, eded, i;
+
+// eded=10;
+// cem=0;
+
+// for(i=1; i<eded; i++){
+//     if(i%2==0){
+//         cem=cem+i;
+//     }
+// }
+// console.log(`Cem: ${cem}`);
+
+// let i=1;
+
+// while(i<10){
+//     console.log(i);
+//     i++
+// }
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+const arr = ["lenkaran", "baki", "gence", "leerik"];
+// console.log(arr.length);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+let axtar = "baki";
+console.log(arr.length);
+
+for (let i = 0; i < arr.length; i++) {
+  if (axtar == arr[i]) {
+    console.log(i);
+  }
+}
