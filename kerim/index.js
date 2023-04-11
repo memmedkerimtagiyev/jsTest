416e8aee5a1ba988d8e8ddf355a1fe98ed398ebb
memmedkerim taghiyev <7m16zu6@code.edu.az>
2023-04-11 16:23:45 +0300

    first comit

--- kerim/index.js
+++ kerim/index.js
@@ -65,16 +65,57 @@
 //     console.log(i);
 //     i++;
 // }while(i<10);
-const arr = ["lenkaran", "baki", "gence", "leerik"];
+// const arr = ["lenkaran", "baki", "gence", "leerik"];
 // console.log(arr.length);
 // for (let i = 0; i < arr.length; i++) {
 //   console.log(arr[i]);
 // }
-let axtar = "baki";
-console.log(arr.length);
+// let axtar = "baki";
+// console.log(arr.length);
 
-for (let i = 0; i < arr.length; i++) {
-  if (axtar == arr[i]) {
-    console.log(i);
+// for (let i = 0; i < arr.length; i++) {
+//   if (axtar == arr[i]) {
+//     console.log(i);
+//   }
+// }
+
+// const arry = [1, 4, 36, 86, 234, 8, 90];
+
+// for (let i = 0; i < arry.length; i++) {
+//   if (arry[i] > 20) {
+//     console.log(i);
+//   }
+// }
+// let cem = 0;
+// for (let i = 1; i < 100; i++) {
+//   cem += i;
+// }
+// console.log(cem);
+
+// let start = 1;
+// let end = 100;
+// let sum = (start + end / 2) * (end / 2);
+// console.log(sum);
+
+// let n = 1000;
+// let s = 0;
+// for (let i = 0; i < 1000; i++) {
+//   if ((i % 3 == 0) / 2) {
+//     s = s + 1;
+//   }
+// }
+// console.log(s);
+
+let arr1 = [1, 2, 3, 4, 5, -6, 7, 78, 32, 36];
+let max = (min = 0);
+
+for (let i = 0; i < arr1.length; i++) {
+  if (arr1[i] > max) {
+    max = arr1[i];
+  }
+  if (arr1[i] < min) {
+    min = arr1[i];
   }
 }
+console.log(max);
+console.log(min);
